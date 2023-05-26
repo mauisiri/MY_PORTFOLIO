3c21cdff8dfb3c7d93f0c7cb714d45977c84be8d
mauisiri <irisleo.com@gmail.com>
2023-05-27 00:03:03 +0200

    profile pic added, but too responsive

--- front/src/components/MyApp/MyApp.jsx
+++ front/src/components/MyApp/MyApp.jsx
@@ -6,42 +6,47 @@ import Css from '../Figures/Css'
 import Js from '../Figures/Js'
 import React_i from '../Figures/React_i'
 import Node from '../Figures/Node'
+import profile_pic from '../../assets/personalPic/irisLeo.png'
 
 const MyApp = () => {
   return (
     <div className='myApp'>
-      <div className="left_side">
-        <Header />
-      
-        <div className="my_app_text">
-          <div>
-            <span className="stroke_text">I am </span>
-            <span className="my_name">Iris Leo</span>
-            <span className="stroke_text">, </span>
-          </div>
-          
-          <div>
-            <span className="subtitle">Software Developer</span>
+      <Header />
+      <div className='main'>
+        <div className="left_side">
+          <div className="my_app_text">
+            <div>
+              <span className="stroke_text">I am </span>
+              <span className="my_name">Iris Leo</span>
+              <span className="stroke_text">, </span>
+            </div>
+            
+            <div>
+              <span className="subtitle">Software Developer</span>
+            </div>
+
+            <div>
+              <span>All progress takes place outside the comfort zone</span> 
+            </div>
           </div>
 
-          <div>
-            <span className='line'>All progress takes place outside the comfort zone</span> 
+          <div className="languages_box">
+            <Html5 />
+            <Css />
+            <Js />
+            <React_i />
+            <Node />
+          </div>
+
+          <div className="the_best_app">
+            <div></div>
+            <span>Check my formation</span>
           </div>
         </div>
 
-        <div className="languages_box">
-          <Html5 />
-          <Css />
-          <Js />
-          <React_i/>
-          <Node/>
+        <div className="right_side">
+          <img className='profile_pic' src={profile_pic} alt=' '/>
         </div>
-
-        <div className="the_best_app">
-          <div></div>
-          <span>Check my formation</span>
-        </div>
-
       </div>
     </div>
   )
