37bf52d81fa4c59c0734a9fa468c321b29a24c24
mauisiri <irisleo.com@gmail.com>
2023-06-19 23:18:07 +0200

    media q main area

--- front/src/components/Home/Home.jsx
+++ front/src/components/Home/Home.jsx
@@ -1,51 +1,53 @@
 import React from 'react'
 import './Home.css'
-import {motion} from 'framer-motion'
+import { motion } from 'framer-motion'
 import profile_pic from '../../assets/personalPic/profile_pic.png'
 
 const Home = () => {
-    const transition = {type: 'spring', duration : 2}
+  const transition = { type: 'spring', duration: 2 }
 
-    return (
-<>
-<div className='main'>
-<div className="left_side">
-  <div className="my_app_text">
-    <div>
-      <span className="stroke_text">I am </span>
-      <span className="my_name">Iris Leo</span>
-      <span className="stroke_text">, </span>
-    </div>
-    
-    <div>
-      <span className="subtitle">Software Developer</span>
-    </div>
+  return (
+    <>
+      <div className='main'>
+        <div className="left_side">
+          <div className="my_app_text">
+            <div>
+              <span className="stroke_text">I am </span>
+              <div>
+                <span className="my_name">Iris Leo</span>
+                <span className="stroke_text">, </span>
+              </div>
+            </div>
 
-    <div>
-      <span>"All progress takes place outside the comfort zone"</span> 
-    </div>
-  </div>
-  <div className="with_btn">
-    <button className="btn_contact_me"></button>
-  
-    <div className="contact_me">
-      <motion.div
-      initial={ {left: '100px'} }
-      whileInView={ {left: '8px'} }
-      transition = {{...transition, type: 'tween'}}
-      ></motion.div>
-      <span>Contact</span>
-      <p>me</p>
-    </div>
-  </div>
-</div>
+            <div>
+              <span className="subtitle">Software Developer</span>
+            </div>
 
-<div className="right_side">
-   <img className='profile_pic' src={profile_pic} alt=' '/> 
-</div>
-</div>
-</>
-)
+            <div>
+              <span className='quote'>"All progress takes place outside the comfort zone"</span>
+            </div>
+          </div>
+          <div className="with_btn">
+            <button className="btn_contact_me"></button>
+
+            <div className="contact_me">
+              <motion.div
+                initial={{ left: '100px' }}
+                whileInView={{ left: '8px' }}
+                transition={{ ...transition, type: 'tween' }}
+              ></motion.div>
+              <span>Contact</span>
+              <p>me</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="right_side">
+          <img className='profile_pic' src={profile_pic} alt=' ' />
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Home
