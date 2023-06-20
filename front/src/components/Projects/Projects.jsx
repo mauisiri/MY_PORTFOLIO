4d75cde647b91cc26a8c82a5bf8be3cc3663f551
mauisiri <irisleo.com@gmail.com>
2023-06-20 09:14:51 +0200

    Responsive main and projects

--- front/src/components/Projects/Projects.jsx
+++ front/src/components/Projects/Projects.jsx
@@ -15,66 +15,71 @@ import weCheck_projects from '../../assets/projects/weCheck/projectsPage.jpeg'
 
 
 const Projects = () => {
-  return (
-    <div className='projects'>
-        <hr></hr>
-        <span className='header'>PROJECTS</span>
-        <div className="container">
-        <div className="projects_container">
-        <div className='project_card'>         
-                <div className='text'>
-                        <span className='project_title'>[adopt/a/coder]</span>
-                        <span className='project_description'>This project was created to connect coders with enterprises which want to "take care of them", since both of them are looking for concret and special -soft and hard- skills and atmosphere.</span>
-                        
-                        
-                        <a href="https://adoptacoder.vercel.app/" target="_blank" rel="noopener noreferrer">
-                        <button className='btn'>Demo</button>
-                        </a>
-                        
-                        <a href="https://github.com/mauisiri/adoptacoder" target="_blank" rel="noopener noreferrer">
-                        <button className='btn'>GitHub</button></a>
-                        <div className='icons_bg'>
-                                <span className='project_icons'>
-                                <Html5 />
-                                <Css />
-                                <Js />
-                                <React_i />
-                                <Node />
-                                </span>
+        return (
+                <div className='projects'>
+                        <hr></hr>
+                        <span className='header'>PROJECTS</span>
+                        <div className="container">
+                                <div className="projects_container">
+                                        <div className='project_card'>
+                                                <div className='text'>
+                                                        <span className='project_title'>[adopt/a/coder]</span>
+                                                        <span className='project_description'>This project was created to connect coders with enterprises which want to "take care of them", since both of them are looking for concret and special -soft and hard- skills and atmosphere.</span>
+
+
+                                                        <a href="https://adoptacoder.vercel.app/" target="_blank" rel="noopener noreferrer">
+                                                                <button className='btn'>Demo</button>
+                                                        </a>
+
+                                                        <a href="https://github.com/mauisiri/adoptacoder" target="_blank" rel="noopener noreferrer">
+                                                                <button className='btn'>GitHub</button></a>
+                                                        <div>
+                                                                <span className='project_icons'>
+                                               <div>          <Html5 />
+                                                                        <Css />
+                                                                        <Js />
+                                                                        </div>        
+                                                                        <div>                                                                    <React_i />
+                                                                        <Node />
+                                                                        </div>                                                                      </span>
+                                                        </div>
+                                                </div>
+                                                <span className='project_img'>
+                                                        <img src={adopt_home} alt=' ' />
+                                                </span>
+                                        </div>
+                                </div>
+                                <div className='project_card'>
+                                        <div className='text'>
+                                                <span className='project_title'>WeCheck</span>
+                                                <span className='project_description'>WeCheck is a rubric system for the critical evaluation of interfaces, this idea was developed by a research group from the Universitat Oberta de Catalunya.
+                                                        Different evaluators can review web pages and score them using this system. The customer needs a website where these rubrics can be entered, so that registered or unregistered users can evaluate websites and obtain a visual result of the evaluations, by evaluator and by average.
+                                                </span>
+
+
+                                                <a href="https://github.com/Rubrics-project/WeCheckUx" target="_blank" rel="noopener noreferrer">
+                                                        <button className='btn'>GitHub</button></a>
+
+                                                <span className='project_icons'>
+                                                        <div>
+                                                                <Js />
+                                                                <React_i />
+                                                                <Node />
+                                                        </div>
+                                                        <div>
+                                                                <Laravel />
+                                                                <MySql />
+                                                                <Php />
+                                                        </div>
+                                                </span>
+                                        </div>
+                                        <span className='project_img'>
+                                                <img src={weCheck_projects} alt=' ' />
+                                        </span>
+                                </div>
                         </div>
                 </div>
-                <span className='project_img'>
-                <img src={adopt_home} alt=' '/>
-                </span>
-        </div>
-        </div>
-        <div className='project_card'>         
-                <div className='text'>
-                        <span className='project_title'>WeCheck</span>
-                        <span className='project_description'>WeCheck is a rubric system for the critical evaluation of interfaces, this idea was developed by a research group from the Universitat Oberta de Catalunya. 
-                        Different evaluators can review web pages and score them using this system. The customer needs a website where these rubrics can be entered, so that registered or unregistered users can evaluate websites and obtain a visual result of the evaluations, by evaluator and by average.
-                        </span>
-
-                        
-                        <a href="https://github.com/Rubrics-project/WeCheckUx" target="_blank" rel="noopener noreferrer">
-                                <button className='btn'>GitHub</button></a>
-
-                        <span className='project_icons'>
-                        <Js />
-                        <React_i />
-                        <Node />
-                        <Laravel />
-                        <MySql />
-                        <Php />
-                        </span>
-                </div>
-                <span className='project_img'>
-                <img src={weCheck_projects} alt=' '/>
-                </span>
-        </div>
-        </div>
-    </div>
-  )
+        )
 }
 
 export default Projects
