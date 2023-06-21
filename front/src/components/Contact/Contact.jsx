7de0667a1b32b1e51abcfd0aa2ab3d04f886e828
mauisiri <irisleo.com@gmail.com>
2023-06-21 09:35:31 +0200

    media query ok

--- front/src/components/Contact/Contact.jsx
+++ front/src/components/Contact/Contact.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import qr_code from '../../assets/qr_code.png'
 import './Contact.css'
 
 
@@ -12,7 +13,12 @@ const Contact = () => {
             <span>BY PHONE
                     <li>+34 747-44-20-20</li>
                     <li>Spain</li>
-                    <li>Also WhatsApp</li>
+                    <li><a href=" https://wa.link/iuoipv" target=""> WhatsApp me</a></li>
+                    <li>
+                        <img className='qu_code' src={qr_code} alt=' '/>
+                   </li>
+
+                   
                    
             </span>
 
@@ -21,8 +27,9 @@ const Contact = () => {
             </span>
 
             <span>MORE OPTIONS
-                    <li>LinkedIn</li>
-                    <li>GitHub</li>
+                    <li><a href="https://www.linkedin.com/in/irisleo/" target="">LinkedIn</a></li>
+                    <li><a href="https://github.com/mauisiri" target="">GitHub</a></li>
+                    
 
             </span>
         </div>
