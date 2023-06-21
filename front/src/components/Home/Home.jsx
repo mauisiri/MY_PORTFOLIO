875500bb16287a02a254c0a453f201d5240e9f7c
mauisiri <irisleo.com@gmail.com>
2023-06-21 11:43:37 +0200

    bug img, x hamburger menu in laptop screen and btn link contact

--- front/src/components/Home/Home.jsx
+++ front/src/components/Home/Home.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Home.css'
 import { motion } from 'framer-motion'
 import profile_pic from '../../assets/personalPic/profile_pic.png'
+import { Link } from "react-scroll";
 
 
 
 const Home = () => {
   const transition = { type: 'spring', duration: 2 }
+
+  const [click,setClick] = useState(false)
+  const handleClick = () => setClick(!click)
+
+  //const closeMenu = () = setClick(false)
 
   return (
     <>
@@ -30,8 +36,16 @@ const Home = () => {
             </div>
           </div>
           <div className="with_btn">
-            <button className="btn_contact_me"></button>
-            <div className="contact_me">
+            <button className="btn_contact_me">
+                  <Link
+                   // onClick={() => closeMenu(true)}
+                    to='contact'
+                    spy={true}
+                    smooth={true}
+                  ></Link>
+
+            </button>
+            <div className="contact_me"> 
               <motion.div
                 initial={{ left: '100px' }}
                 whileInView={{ left: '8px' }}
