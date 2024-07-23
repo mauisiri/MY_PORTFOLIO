40d0bf48caad41e8f6f136de20407e50e0eace8d
Iris Leo <iris.leocallealta.extern@macif.fr>
2024-07-23 08:29:47 +0200

    MODIFIED: header menu transparency modified to be more opac (less transparent)

--- front/src/components/Interaction/CountDown/TimerChallenge.jsx
+++ front/src/components/Interaction/CountDown/TimerChallenge.jsx
@@ -1,17 +1,35 @@
+import { useState } from 'react';
+
 export default function TimerChallenge({title, targetTime}) {
+
+    const [timerStarted, setTimerStarted] = useState(false);
+const [timerExpired, setTimerExpired] = useState(false);
+
+    function handleStart() {
+        setTimeout(() => 
+            {setTimerExpired(true);},
+            targetTime * 1000);
+            setTimerStarted(true);
+    }
+
+    function handleStop() {
+        
+    }
+    
     return (
         <section className='countDownChallenge'>
             <h2>{title}</h2>
+            {timerExpired && <p>Try it again!</p>}
             <p className='countDownChallenge-time'>
                 {targetTime} second{targetTime > 1 ? 's' : ''}
             </p>
             <p>
-                <button>
-                    Start Challenge
+                <button onClick={handleStart}>
+                    {timerStarted ? 'Stop' : 'Start'} Challenge
                 </button>
             </p>
-            <p className=''>
-                Time is running... /Timer inactive
+            <p className={timerStarted ? 'active' : undefined}>
+                {timerStarted ? 'Time is running...' : 'Timer inactive'}
             </p>
         </section>
     )
