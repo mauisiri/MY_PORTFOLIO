024d5110ac20507d3712f03a4891e3cf817f4697
Iris Leo <iris.leocallealta.extern@macif.fr>
2024-07-30 08:43:32 +0200

    ADDED: Skills. Ready to push to main.

--- front/src/components/Skills/Skills.jsx
+++ front/src/components/Skills/Skills.jsx
@@ -3,48 +3,48 @@ import './Skills.css'
 import SectionTitle from '../SectionTitle/SectionTitle';
 
 
-
-
-
 const Skills = () => {
   return (
     <div className='skills'>
       <SectionTitle title="SKILLS"/>
         <div className='content'>
-            <span>Languages
-                    <li>JavaScript</li>
-                    <li>HTML5/CSS3</li>
-                    <li>SQL</li>
-                    <li>PHP</li>
-            </span>
 
-            <span>FrontEnd frameworks     
-                    <li>React</li>
-                    <li>Vue</li>
-                    <li>Bootstrap</li>
+            <span>FrontEnd
+                <li>JavaScript</li>    
+                <li>HTML5/CSS3</li>
+                <li>Bootstrap</li>
+                <li>Typescript</li>
+                <li>React</li>
+                <li>Vue</li>
+                <li>JUnit</li>
+                <li>Figma, PhotoShop</li>
             </span>
 
             <span>BackEnd
-                    <li>Node</li>
-                    <li>PHP</li>
-                    <li>SQL</li>
-                    <li>MySQL</li>
-                    <li>Postman</li>
-                    <li>Laravel</li>
-                    <li>MVC</li>
-                    <li>API Rest</li>
+                <li>JavaScript</li>
+                <li>Node</li>
+                <li>PHP</li>
+                <li>SQL</li>
+                <li>MySQL</li>
+                <li>Postman</li>
+                <li>Laravel</li>
+                <li>MVC</li>
+                <li>API Rest</li>
             </span>
 
-            <span>Agile, tools & good practices
-                    <li>SCRUM & Kanban</li>
-                    <li>SOLID</li>
-                    <li>OOP</li>
-                    <li>Atomic Design</li>
-                    <li>Git & GitHub</li>
-                    <li>Figma</li>
-                    <li>PhotoShop</li>
-                    <li>Trello, Asana & ClickUp</li>
-                    <li>Slack</li>
+            <span>Tools & good practices
+                <li>Agile</li>
+                <li>SCRUM & Kanban</li>
+                <li>SOLID</li>
+                <li>OOP</li>
+                <li>MVC</li>
+                <li>Microservices</li>
+                <li>Atomic Design</li>
+                <li>Chat GPT / Gemini</li>
+                <li>Slack, Teams</li>
+                <li>Git & GitHub / Bitbucket</li>  
+                <li>Trello, Asana & ClickUp</li>
+                
             </span>
         </div>
     </div>
