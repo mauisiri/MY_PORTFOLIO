475189f87bbf834ee423b4973e0a1272a5d96bc3
Iris Leo <iris.leocallealta.extern@macif.fr>
2024-08-07 13:24:23 +0200

    FIXED: time callenger css responsive

--- src/components/Interaction/CountDown/CountDownRender.jsx
+++ src/components/Interaction/CountDown/CountDownRender.jsx
@@ -5,22 +5,22 @@ import TimerChallenge from "./TimerChallenge";
 
 const CountDownRender = () => {
   return (
-   
     <React.StrictMode>
       <>
-      
-      <section className="countDownbody">
-      <CountDown />
-        <TimerChallenge title='Easy' targetTime={1}/>
-        <TimerChallenge title='Not easy' targetTime={5}/>
-        <TimerChallenge title='Getting tough' targetTime={10}/>
-        <TimerChallenge title='Pros only' targetTime={15}/>
-      </section>
+        <section className="countDownbody">
+          <div className="countDown-container">
+            <CountDown />
+          </div>
+          <div className="countDownChallenges-container">
+            <TimerChallenge title="Easy" targetTime={1} />
+            <TimerChallenge title="Not easy" targetTime={5} />
+            <TimerChallenge title="Getting tough" targetTime={10} />
+            <TimerChallenge title="Pros only" targetTime={15} />
+          </div>
+        </section>
       </>
     </React.StrictMode>
-    
-   
   );
-}
+};
 
 export default CountDownRender;
